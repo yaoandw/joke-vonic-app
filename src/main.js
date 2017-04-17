28e6b25b7f30662988306618b43d7365a626593a
yaoandw <yaoandw@163.com>
2017-04-17 17:19:45 +0800

    update to vue-webpack-starter template

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
-import Vonic from 'vonic/src/index.js'
+import Vonic from 'vonic'
 import AV from 'leancloud-storage/dist/node/index.js'
 
 var APP_ID = 'eq5tge5ziB5wPQdu1WJyy3O2';
 var APP_KEY = 'EjyBMtW8AJatUGpw6mKaszPR';
 AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
+  appId: APP_ID,
+  appKey: APP_KEY
 });
-
 // Page Components
 import Index from './components/Index.vue'
 import About from './components/About.vue'
